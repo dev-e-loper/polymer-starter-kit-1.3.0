0007b8159fc1e7843029f5f2fc95adff6e73e2a6
dev-e-loper <levon@amelyan.com>
2016-07-09 09:31:08 -0500

    Add fab button element
    Just for testing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+    app.baseUrl = '/dev-e-loper/polymer-starter-kit-1.3.0/';
   }
 
   app.displayInstalledToast = function() {
